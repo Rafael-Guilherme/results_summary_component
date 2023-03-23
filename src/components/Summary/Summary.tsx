a4a2c8654e8a5f489ea25329792ff00aaa9e9a7c
Rafael <rafael_guilherme@hotmai.com>
2023-03-23 11:47:03 -0300

    final

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,53 +1,26 @@
-import React from 'react'
 import data from '../../data/data.json';
 import icon from '../../assets/images/'
 import '../Summary/summary.scss'
 
 const Summary = () => {
+  const myData = data
+
   return (
     <div className="summary-charts">
       <div className="summary-charts-container">
         <h2 className="summary-title">Summary</h2>
-        <div className="status">
-          <div className="status-title">
-            <img className="status-title-img" src={icon.iconReaction} />
-            <p className="reaction">Reaction</p>
+        {myData.map(item => (
+          <div className="status" key={item.category}>
+            <div className="status-title">
+              <img className="status-title-img" src={item.icon} alt={item.category} />
+              <p className={`category-${item.category}`}>{item.category}</p>
+            </div>
+              <p>
+                <span className="number">{item.score} </span>
+                <span className="number2">/ 100</span>
+              </p>
           </div>
-          <p>
-            <span className="number">80 </span>
-            <span className="number2">/ 100</span>
-          </p>
-        </div>
-        <div className="status">
-          <div className="status-title">
-            <img className="status-title-img" src={icon.iconMemory} />
-            <p className="memory">Memory</p>
-          </div>
-          <p>
-            <span className="number">92 </span>
-            <span className="number2">/ 100</span>
-          </p>
-        </div>
-        <div className="status">
-          <div className="status-title">
-            <img className="status-title-img" src={icon.iconVerbal} />
-            <p className="verbal">Verbal</p>
-          </div>
-          <p>
-            <span className="number">61 </span>
-            <span className="number2">/ 100</span>
-          </p>
-        </div>
-        <div className="status">
-          <div className="status-title">
-            <img className="status-title-img" src={icon.iconVisual} />
-            <p className="visual">Visual</p>
-          </div>
-          <p>
-            <span className="number">72 </span>
-            <span className="number2">/ 100</span>
-          </p>
-        </div>
+        ))}
         <button className="summary-button" type="button">Continue</button>
       </div>
     </div>
